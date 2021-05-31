bfcd9ad96572558115c5a3920c8db586504d5d77
Enrique Bermúdez <enrique.bermudez@mercadolibre.com>
2021-05-31 17:51:40 -0300

    fix: update docs path

--- docusaurus/website/docusaurus.config.js
+++ docusaurus/website/docusaurus.config.js
@@ -37,6 +37,7 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
+          path: '../docs',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
