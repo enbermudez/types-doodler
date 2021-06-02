e491e028e4bf46b3d9f003dd69827a398d18e851
Enrique Bermúdez <enrique.bermudez@mercadolibre.com>
2021-06-02 11:46:29 -0300

    feat: update test validations

--- src/strings/weird-case.js
+++ src/strings/weird-case.js
@@ -3,7 +3,7 @@ import validator from '../validator';
 
 const schema = Joi.object({
   str: Joi.string().min(1).required(),
-  separator: Joi.string().max(1).default(' ')
+  separator: Joi.string().length(1).max(1).default(' ')
 });
 
 export default (fields) => {
