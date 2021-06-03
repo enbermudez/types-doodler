6b3045dc9306d3ff96b362ad19852fb323c07583
Enrique Bermúdez <enrique.bermudez@mercadolibre.com>
2021-06-03 11:13:12 -0300

    fix: add anagram to strings index

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,9 +1,11 @@
+import anagram from './anagram';
 import charmap from './charmap';
 import weirdCase from './weird-case';
 import palindrome from './palindrome';
 import reverse from './reverse';
 
 export default {
+  anagram,
   charmap,
   weirdCase,
   palindrome,
